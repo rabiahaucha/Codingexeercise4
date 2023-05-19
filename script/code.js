632e991bee765e2b4e4e16a8f377696d901fe4fe
rabia <rabiahaucha9@gmail.com>
2023-05-19 09:53:49 +0200

    newstuff

---
+++ script/code.js
@@ -0,0 +1,24 @@
+let mammals = {
+    first: 'whales',
+second : 'monkey',
+last : 'cats',
+
+};
+
+let carnivores = {
+    first:'polar bear',
+    second : 'tiger',
+    last: 'lion',
+
+};
+console.log(mammals);
+console.log(carnivores);
+
+
+let newAnimals = [carnivores]
+console.log(newAnimals);
+
+
+
+
+
