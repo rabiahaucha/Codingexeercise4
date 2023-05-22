6102ad093bcde7309eef274958054a75ca7650df
rabia <rabiahaucha9@gmail.com>
2023-05-22 14:04:14 +0200

    calculator

--- script/code.js
+++ script/code.js
@@ -1,24 +1,41 @@
-let mammals = {
-    first: 'whales',
-second : 'monkey',
-last : 'cats',
+// let mammals = {
+//     first: 'whales',
+// second : 'monkey',
+// last : 'cats',
 
-};
+// };
 
-let carnivores = {
-    first:'polar bear',
-    second : 'tiger',
-    last: 'lion',
+// let carnivores = {
+//     first:'polar bear',
+//     second : 'tiger',
+//     last: 'lion',
 
-};
-console.log(mammals);
-console.log(carnivores);
+// };
+// console.log(mammals);
+// console.log(carnivores);
+
+// let array = [];
+
+// array.push(mammals, carnivores);
+
+// console.log(array);
 
 
-let newAnimals = [carnivores]
-console.log(newAnimals);
+// let newAnimals = [carnivores, mammals]
+// console.log(newAnimals);
 
 
+//parameter//
+var first = 5;
+var second = 7;
+[first, second] = [second, first];
+console.log(first, second);
 
+function swap(me,you) {
+    return [you,me];
+}
+
+console.log(swap(15,17));
+// argument//
 
 
